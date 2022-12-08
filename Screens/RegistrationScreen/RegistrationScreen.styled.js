c2a9eeaf0b61cbfcf8fa96edca8400af44f29166
Iuliia Dolnikova <dolnikova80@gmail.com>
2022-12-09 01:14:18 +0200

    add

--- Screens/RegistrationScreen/RegistrationScreen.styled.js
+++ Screens/RegistrationScreen/RegistrationScreen.styled.js
@@ -35,7 +35,7 @@ export const styles = StyleSheet.create({
     marginBottom: 32,
     paddingVertical: 8,
     textAlign: "center",
-    // color: "#212121",
+
     color: "#red",
     fontSize: 30,
   },
